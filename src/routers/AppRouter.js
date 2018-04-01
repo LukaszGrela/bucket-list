31584ae84f04ee4e1b7bda84916222555916d26e
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-04-01 01:25:31 +0100

    Removed Home as it is unused.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Page404 from '../pages/Page404';
 import Home from '../pages/Home';
 import DragNote from '../pages/DragNote';
@@ -7,9 +7,10 @@ import DragNote from '../pages/DragNote';
 const AppRouter = () => (
     <BrowserRouter>
         <div className='app'>
-            <header>Drag a Note</header>
             <Switch>
-                <Route path="/" exact component={Home} />
+                <Route exact path="/(index.html)?" component={() => {
+                    return <Redirect to='/app' />
+                }} />
                 <Route path="/app" exact component={DragNote} />
                 <Route component={Page404} />
             </Switch>
