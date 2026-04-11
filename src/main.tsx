b87bdcd6a9bad8ae2c1697187d466a3ef4276997
LukaszGrela <lukasz.grela@gmail.com>
2026-04-11 20:21:08 +0200

    update stores

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
+import "./styles/index.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
-import configureStore from "./store/configureStore";
-import "./styles/index.scss";
+import { store } from "./store";
 
-const store = configureStore();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,5 +1,7 @@
+import type { ThunkDispatch } from "redux-thunk";
 import type { store } from "./index";
+import type { Action } from "redux";
 
 export type TStore = typeof store;
 export type TRootState = ReturnType<typeof store.getState>;
-export type TAppDispatch = typeof store.dispatch;
+export type TAppDispatch = ThunkDispatch<TRootState, unknown, Action>;
