7cd0a306559f4b865588e836ea308027e209b0a2
LukaszGrela <lukasz.grela@gmail.com>
2026-04-11 19:15:35 +0200

    some types

--- src/components/dragnote/types.ts
+++ src/components/dragnote/types.ts
@@ -1,10 +1,22 @@
 export interface INote {
-    id: string;
-    bucketId: string;
-    [key: string]: unknown;
+  id: string;
+  bucketId: string;
+  body: string;
+
+  //   created_at;
+  //   owner: userid;
 }
 
 export interface INotes {
-    list: INote[];
-    loading?: boolean;
+  list: INote[];
+  loading?: boolean;
+}
+
+export interface IBucket {
+  id: string;
+
+  name: string;
+
+  //   created_at;
+  //   owner: userid;
 }
